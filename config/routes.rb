b59cf6d600a31fdf969ae219e9d589d2d3441166
geermc4 <geermc4@gmail.com>
2013-10-16 10:29:30 -0700

    fixing route for rails 4

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Spree::Core::Engine.routes.append do
   namespace :admin do
     resources :user_groups
   end
-    match '/admin/user_groups/:id/pricing' => 'admin/userGroups#pricing', :as => 'user_group'
+  get '/admin/user_groups/:id/pricing' => 'admin/user_groups#pricing', :as => 'user_group'
 end
