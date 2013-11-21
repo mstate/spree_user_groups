19a7c6eb1c5d6b440b830b24e4390f90d60accfd
Ramón Roche <mrpollo@gmail.com>
2013-11-21 15:24:09 -0800

    user distro icon in user show

--- app/overrides/add_user_group_icons.rb
+++ app/overrides/add_user_group_icons.rb
@@ -1,11 +1,23 @@
-star_icon = <<TEXT
+icon_class = 'icon-star no-text distro-star-icon'
+edit_star_icon = <<TEXT
 <% if user.user_group.present? %>
-  &nbsp; <%=link_to('', object_url(user), {:class => 'icon-star no-text'}) %>
+  &nbsp; <%= link_to('', object_url(user), {:class => '#{icon_class}'}) %>
+<% end %>
+TEXT
+show_star_icon = <<TEXT
+<% if @user.user_group.present? %>
+  &nbsp; <span class='#{icon_class}'>&nbsp;</span>
 <% end %>
 TEXT
 Deface::Override.new(
-  :virtual_path=> "spree/admin/users/index",
-  :name=> "user_listing_icon",
+  :virtual_path => "spree/admin/users/index",
+  :name => "user_listing_icon",
   :insert_bottom => "td.user_email",
-  :text=> star_icon,
+  :text => edit_star_icon,
+)
+Deface::Override.new(
+  :virtual_path => "spree/admin/users/show",
+  :name => "user_show_icon",
+  :insert_after => "[erb-loud]:contains('@user.email')",
+  :text => show_star_icon,
 )
