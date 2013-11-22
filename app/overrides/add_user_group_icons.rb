b3507584c4dca2e4d6c93debbbfc1e0760c912c8
Ramón Roche <mrpollo@gmail.com>
2013-11-21 16:51:27 -0800

    user group icon on order edit tabs

--- app/overrides/add_user_group_icons.rb
+++ app/overrides/add_user_group_icons.rb
@@ -9,15 +9,29 @@ show_star_icon = <<TEXT
   &nbsp; <span class='#{icon_class}'>&nbsp;</span>
 <% end %>
 TEXT
+order_edit_user_icon = <<TEXT
+<dt><%= I18n.t(:user_groups) %>:</dt>
+<dd id="user_group_name">
+  <% if @order.user.present? && @order.user.user_group.present? %>
+    <span class='#{icon_class}'><%= @order.user.user_group.name %></span>
+  <% end %>
+</dd>
+TEXT
 Deface::Override.new(
   :virtual_path => "spree/admin/users/index",
-  :name => "user_listing_icon",
+  :name => "user_listing_user_group_icon",
   :insert_bottom => "td.user_email",
   :text => edit_star_icon,
 )
 Deface::Override.new(
   :virtual_path => "spree/admin/users/show",
-  :name => "user_show_icon",
+  :name => "user_show_user_group_icon",
   :insert_after => "[erb-loud]:contains('@user.email')",
   :text => show_star_icon,
+)
+Deface::Override.new(
+  :virtual_path   => "spree/admin/shared/_order_tabs",
+  :name           => "order_edit_user_group_icon",
+  :insert_before  => "dt#order_status",
+  :text           => order_edit_user_icon
 )
