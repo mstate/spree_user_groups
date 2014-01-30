f80a4e54f44b5cf3c789d266ed8137a7c3f26f4a
Ruben Ascencio <galateaweb@gmail.com>
2014-01-29 16:22:16 -0800

    Remove before_address override
    Not necessary, also the method has changed in spree
    2.1 to support persisting user's addresses.

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -2,10 +2,6 @@ Spree::CheckoutController.class_eval do
   before_filter :verify_minimum_order
 
   private
-  def before_address
-    @order.bill_address ||= Spree::Address.default
-    @order.ship_address ||= Spree::Address.default
-  end
 
   def verify_minimum_order
     if @order.user_id then
