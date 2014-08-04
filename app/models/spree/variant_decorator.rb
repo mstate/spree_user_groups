f4c2ee5377720e291882b42dbb9aa901301fe909
geermc4 <geermc4@gmail.com>
2014-08-04 15:40:55 -0700

    default to list price if no special price added for group

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -3,18 +3,16 @@ Spree::Variant.class_eval do
 
   def price_for_user(user)
     if user && user.user_group
-      user.user_group.price_for_variant(self)
+      user.user_group.price_for_variant(self) || price
     else
       price
     end
   end
 
   def price
-    if Spree::User.current.nil? or Spree::User.current.user_group.nil? then
-      return prices.first.amount.to_f unless prices.first.blank? || prices.first.amount.blank?
-      return product.prices.first.amount.to_f unless product.blank? || product.prices.first.blank?
-      return 0
-    end
+    return prices.first.amount.to_f unless prices.first.blank? || prices.first.amount.blank?
+    return product.prices.first.amount.to_f unless product.blank? || product.prices.first.blank?
+    return 0
   end
 
   #TODO-Proper fix for this hack :)
